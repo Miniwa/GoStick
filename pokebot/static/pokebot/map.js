1fefbc230a62b7f63ae124b6f0c380a8a6e6c053
Miniwa <blarch3030@gmail.com>
2016-07-21 11:22:57 +0200

    Reorganized the pokebot API a bit. Made the API into a stateful object instead of a module. Also moved function definitions out.

---
+++ pokebot/static/pokebot/map.js
@@ -0,0 +1,15 @@
+var MapPage = (function()
+{
+        return {
+
+            Initialize: function()
+            {
+                var coord = new PB.Coordinate(60, 15);
+                var bot = new PB.Pokebot("pokebot-map", coord);
+                bot.setTarget(coord);
+                bot.setPosition(new PB.Coordinate(60, 14));
+            },
+        };
+})();
+
+MapPage.Initialize();
--- pokebot/static/pokebot/pokebot.js
+++ pokebot/static/pokebot/pokebot.js
@@ -1,55 +1,117 @@
-var PB = (function()
-{
-	//Public API
-	return {
-		
-		//Point definition.
-		Point: function(lat, lon)
-		{
-			//Calculate the distance between two points.
-			this.Dist = function(rhv)
-			{
-				return Math.abs(rhv.getLatitude() - this.getLatitude()) +
-							Math.abs(rhv.getLongitude() - this.getLongitude());
-			},
-			
-			//Set the latitude.
-			this.setLatitude = function(lat)
-			{
-				this._lat = lat;
-			};
-			
-			//Get latitude.
-			this.getLatitude = function()
-			{
-				return this._lat;
-			};
-			
-			//Set longitude
-			this.setLongitude = function(lon)
-			{
-				this._lon = lon
-			};
-			
-			//Get longitude.
-			this.getLongitude = function()
-			{
-				return this._lon;
-			};
-			
-			//Construct the object.
-			var localLat = lat || 0;
-			var localLon = lon || 0;
-			
-			if(localLat > 90 || localLat < -90)
-				throw new Error("Latitude not within boundaries.");
-			
-			if(localLon > 180 || localLon < -180)
-				throw new Error("Longitude not within boundaries.");
-			
-			//Construct the object.
-			this.setLatitude(localLat);
-			this.setLongitude(localLon);
-		},
-	}
+var PB = (function() {
+
+    //Public API
+    return {
+        //Coordinate declaration.
+        Coordinate: function(lat, lng) {
+            var localLat = lat || 0;
+            var localLng = lng || 0;
+
+            if (localLat > this.LAT_MAX || localLat < this.LAT_MIN)
+                throw new Error("Latitude not within boundaries.");
+
+            if (localLng > this.LNG_MAX || localLng < this.LNG_MIN)
+                throw new Error("Longitude not within boundaries.");
+
+            //Construct the object.
+            this.setLatitude(localLat);
+            this.setLongitude(localLng);
+        },
+
+        //Pokebot declaration.
+        //Initializes the pokebot with the camera pointed at the coordinates specified by coord.
+        Pokebot: function(selector, coord) {
+            coord = coord || new Coordinate(0, 0);
+
+            this._markers = [];
+            this._target = null;
+            this._position = null;
+
+            this._map = new google.maps.Map(document.getElementById(selector), {
+                center: {
+                    lat: coord.getLatitude(),
+                    lng: coord.getLongitude(),
+                },
+                zoom: 10
+            });
+        },
+    };
 })();
+
+//Coordinate definitions.
+PB.Coordinate.prototype.LAT_MAX = 90;
+PB.Coordinate.prototype.LAT_MIN = -90;
+PB.Coordinate.prototype.LNG_MAX = 180;
+PB.Coordinate.prototype.LNG_MIN = -180;
+
+//Calculate the distance between two coordinates.
+PB.Coordinate.prototype.Dist = function(rhv) {
+    return Math.abs(rhv.getLatitude() - this.getLatitude()) +
+        Math.abs(rhv.getlnggitude() - this.getlnggitude());
+};
+
+//Set latitude.
+PB.Coordinate.prototype.setLatitude = function(lat) {
+    this._lat = lat;
+};
+
+//Get latitude.
+PB.Coordinate.prototype.getLatitude = function() {
+    return this._lat;
+};
+
+//Set longitude
+PB.Coordinate.prototype.setLongitude = function(lng) {
+    this._lng = lng;
+};
+
+//Get longitude.
+PB.Coordinate.prototype.getLongitude = function() {
+    return this._lng;
+};
+
+
+//Pokebot definitions.
+//Sets the active target.
+PB.Pokebot.prototype.setTarget = function(coord) {
+    this._target = coord;
+    this.drawMarker(coord, "Target");
+};
+
+PB.Pokebot.prototype.getTarget = function()
+{
+    return this._target;
+};
+
+PB.Pokebot.prototype.setPosition = function(coord)
+{
+        this._position = coord;
+        this.drawMarker(coord, "Position");
+};
+
+PB.Pokebot.prototype.setCamera = function(coord)
+{
+    this._map.setPosition({
+        lat: coord.getLatitude(),
+        lng: coord.getLongitude(),
+    });
+};
+
+PB.Pokebot.prototype.getCamera = function()
+{
+    var pos = this._map.getPosition();
+    return new PB.Coordinate(pos.lat(), pos.lng());
+};
+
+PB.Pokebot.prototype.drawMarker = function(coord, title)
+{
+    title = title || "";
+    this._markers.push(new google.maps.Marker({
+        map: this._map,
+        position: {
+            lng: coord.getLongitude(),
+            lat: coord.getLatitude()
+        },
+        title: title
+    }));
+};
